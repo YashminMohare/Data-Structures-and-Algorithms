2a07a3840d78563d5f3af53338baf9cf5fde9809
Yashmin Mohare <145188295+YashminMohare@users.noreply.github.com>
2026-09-09 11:38:50 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.55%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] row= new int [n];
+        Arrays.fill(row, 1);
+
+        // loop m-1 time to build upper row
+        for(int i = 0; i< m-1; i++) {
+            int [] newRow = new int[n];
+            Arrays.fill(newRow, 1);
+
+            for(int  j = n-2 ; j>=0; j--) {
+                newRow[j] = newRow[j+1] + row[j];
+            }
+            row = newRow;
+        }
+        return row[0];
+    }
+}
